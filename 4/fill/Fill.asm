570c17e262c0d2de98f9338a3141ab1f6261055f
wuningxi <wuningxi>
2024-05-05 12:55:51 +0200

    Add code for week 4

--- 4/fill/Fill.asm
+++ 4/fill/Fill.asm
@@ -8,4 +8,119 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+//// part 1: detect key press
+// prev = 0
+// while True:
+//		if keyboard!=0:
+//			pressed=-1
+//		else:
+//			pressed=0
+// 		if pressed!=prev:
+//			paint(colour=pressed)
+//			
+//// part 2: paint screen
+// def paint(colour):
+// 		for i in range(screen,KBD-1):
+//			RAM[i]=colour
+
+
+@white
+M=0
+
+@black
+M=-1
+
+@prevkey
+M=0
+
+
+// keyboard monitoring part
+
+(KBDLOOP)
+	@KBD //24578
+	D=M
+
+	@pressedkey // store currently pressed key
+	M=D
+
+	@SETWHITE
+	D; JEQ // if no key pressed, set colour=white 
+
+	@SETBLACK
+	0; JMP // otherwise set colour=black
+
+	(CONTINUE)
+	@prev
+	D=M
+	@colour
+	D=D-M 
+	@change
+	M=D // change = prev==colour
+
+	@colour
+	D=M
+	@prev
+	M=D // prev = colour
+
+	@change
+	D=M
+
+	@RESET
+	D; JNE // go to paint loop if colour has changed
+
+	@KBDLOOP // go to start of loop
+	0; JMP
+
+
+// screen painting part
+
+(SETWHITE)
+	@colour
+	M=0
+	@CONTINUE
+	0; JMP
+
+(SETBLACK)
+	@colour
+	M=-1
+	@CONTINUE
+	0; JMP
+
+
+(RESET)
+	@SCREEN //16384
+	D=A
+
+	@cur
+	M=D //cur=SCREEN
+
+
+(PAINTLOOP)
+	@KBD //24578
+	D=A
+
+	@cur
+	D=D-M
+
+	@KBDLOOP
+	D; JEQ // stop if reached end of screen (cur-KBD==0)
+
+	@colour
+	D=M
+
+	@cur
+	A=M 
+	M=D // RAM[cur]=colour (0 or -1)
+
+	@cur
+	D=M+1
+	M=D // cur+=1
+
+	@PAINTLOOP
+	0; JMP // go to start of loop
+
+
+
+(END)
+	@END
+	0; JMP
--- 4/mult/Mult.asm
+++ 4/mult/Mult.asm
@@ -7,4 +7,65 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// n = R0
+// f = R1
+// prod = 0
+// 
+// LOOP:
+// 		if i>n: goto stop
+// 		prod = prod+f
+//		goto LOOP
+// 
+// R2=prod
+//
+// STOP
+
+@prod
+M=0 //prod=0
+
+@R0
+D=M
+@n
+M=D //n=R0
+
+@R1
+D=M
+@f
+M=D //f=R1
+
+@i
+M=0  //i=0
+
+@R2
+M=0  //initialise res as 0 in case there are no; only necessary if prematurely stopped as in test script
+
+(LOOP)
+	@i
+	D=M
+	@n
+	D=M-D
+	@STOP
+	D;JEQ	// if i>n goto STOP
+
+	@f
+	D=M
+	@prod
+	M=D+M  // prod=prod+f
+
+	@i
+	M=M+1  // i+=1
+
+	@LOOP
+	0;JMP  // otherwise go to beginning of LOOP
+
+
+(STOP)
+@prod
+D=M
+@R2
+M=D  // R2=prod
+
+(END)
+	@END
+	0;JMP
+
